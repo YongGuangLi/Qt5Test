ae73efd3b7fbcb7e0fa5fadc394f689abc464e28
LIYONGGUANG <liyg0428@icloud.com>
2021-03-11 16:53:04 +0800

    first

---
+++ dialog.cpp
@@ -0,0 +1,42 @@
+﻿#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),running(true),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+    ui->lineEdit->setText(QString::fromLocal8Bit("中国"));
+
+    this->installEventFilter(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+bool Dialog::eventFilter(QObject *o, QEvent *e)
+{
+    if(QEvent::Close == e->type())
+        running = false;
+
+    return QObject::eventFilter(o, e);
+}
+
+void Dialog::slot_calculate(int i)
+{
+    qDebug()<<QThread::currentThreadId();
+    while (running)
+    {
+        ++i;
+        qDebug()<<i;
+        QCoreApplication::processEvents();
+    }
+}
+
+void Dialog::on_pushButton_clicked()
+{
+    QMetaObject::invokeMethod(this, "slot_calculate", Qt::QueuedConnection, Q_ARG(int, 1));
+    qDebug()<<QThread::currentThreadId();
+}
---
+++ dialog.h
@@ -0,0 +1,32 @@
+﻿#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QDebug>
+#include <QThread>
+#include <QSqlDatabase>
+#include <QEvent>
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = nullptr);
+    ~Dialog();
+
+    bool eventFilter(QObject *o, QEvent *e);
+private slots:
+    void slot_calculate(int i);
+    void on_pushButton_clicked();
+
+private:
+    Ui::Dialog *ui;
+    bool running;
+};
+
+#endif // DIALOG_H
